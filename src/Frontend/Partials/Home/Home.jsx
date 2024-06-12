2ef71292eac31b3bfc5c1a85c933afe624f6b877
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-12 12:50:59 +0600

    creating tag section

--- src/Frontend/Partials/Home/Home.jsx
+++ src/Frontend/Partials/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Title from "../../../Components/Title"
+import Tag from "./Tag"
 
 const Home = () => {
     return (
         <div>
             <Title title={'Airbnb'} />
+            {/* Tag section start */}
+            <Tag></Tag>
+            {/* Tag section end */}
 
             <div className="container mx-auto">
                 <div className="grid grid-cols-5 gap-2">
