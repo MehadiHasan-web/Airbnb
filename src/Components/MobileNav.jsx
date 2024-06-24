f4351057d7773c5e0c3a70623d9e51e9c8ef0eb8
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-24 14:01:24 +0600

    try to solve active link

--- src/Components/MobileNav.jsx
+++ src/Components/MobileNav.jsx
@@ -2,34 +2,61 @@ import { useState } from "react";
 import { FaHome } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
-
 const MobileNav = () => {
+  const [active, setActive] = useState(false);
 
-  const [active, setActive] = useState(false)
-
-  console.log(active)
+  const listItems = [
+    {
+      id:1,
+      name: 'Home',
+      path: '/',
+      icon: <FaHome></FaHome>
+    },
+    {
+      id:2,
+      name: 'Contact',
+      path: '/contact',
+      icon: <FaHome></FaHome>
+    },
+    {
+      id:3,
+      name: 'Show',
+      path: '/show',
+      icon: <FaHome></FaHome>
+    },
+  ]
 
   return (
     <div className="md:hidden mt-5 h-16 flex items-center justify-center w-full">
       {/* content section start */}
       <div className="bg-slate-300 px-4 h-full w-full">
         <ul className="flex justify-around items-center h-full w-full">
-            {/* item */}
-              <li onClick={() => setActive(!active)} className={`${active ? 'm-0 duration-300': '-mt-10 duration-300'}`}>
-              <NavLink to="/">
-                <FaHome className={`${active ? 'w-10 h-10' :' bg-pink-400 text-white border-[6px] border-white duration-300 w-14 h-14 '} mx-auto  p-1 rounded-full`}></FaHome>
-                <p className={`${active ? 'hidden duration-300' : 'visible duration-300 mt-1'} text-center font-bold text-sm text-white`}>Home</p>
-              </NavLink>
-              </li>
-              {/* item */}
-            {/* item */}
-              <li onClick={() => setActive(!active)} className={`${active ? 'm-0 duration-300': '-mt-10 duration-300'}`}>
-              <NavLink to="/">
-                <FaHome className={`${active ? 'w-10 h-10' :' bg-pink-400 text-white border-[6px] border-white duration-300 w-14 h-14 '} mx-auto  p-1 rounded-full`}></FaHome>
-                <p className={`${active ? 'hidden duration-300' : 'visible duration-300 mt-1'} text-center font-bold text-sm text-white`}>Home</p>
-              </NavLink>
-              </li>
-              {/* item */}
+          {/* item */}
+          {
+            listItems.map((data, index) => <li key={index}
+            onClick={() => setActive(!active)}
+            className={`${active ? "m-0 duration-300" : "-mt-10 duration-300"}`}
+          >
+            <NavLink to="/">
+              <FaHome
+                className={`${
+                  active
+                    ? "w-10 h-10"
+                    : " bg-pink-400 text-white border-[6px] border-white duration-300 w-14 h-14 "
+                } mx-auto  p-1 rounded-full`}
+              ></FaHome>
+              <p
+                className={`${
+                  active ? "hidden duration-300" : "visible duration-300 mt-1"
+                } text-center font-bold text-sm text-white`}
+              >
+                Home
+              </p>
+            </NavLink>
+          </li>)
+          }
+          
+          {/* item */}
         </ul>
       </div>
       {/* content section end */}
