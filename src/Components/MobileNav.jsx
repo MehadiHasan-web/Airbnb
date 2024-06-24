09e71745e74f160816be203d2388726a6a9d3e23
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-24 15:52:21 +0600

    problem solving onClick function

--- src/Components/MobileNav.jsx
+++ src/Components/MobileNav.jsx
@@ -3,7 +3,9 @@ import { FaHome } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 const MobileNav = () => {
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState(1);
+
+  console.log(active)
 
   const listItems = [
     {
@@ -34,23 +36,22 @@ const MobileNav = () => {
           {/* item */}
           {
             listItems.map((data, index) => <li key={index}
-            onClick={() => setActive(!active)}
+            onClick={() => setActive(data.id)}
             className={`${active ? "m-0 duration-300" : "-mt-10 duration-300"}`}
           >
             <NavLink to="/">
-              <FaHome
-                className={`${
-                  active
-                    ? "w-10 h-10"
-                    : " bg-pink-400 text-white border-[6px] border-white duration-300 w-14 h-14 "
-                } mx-auto  p-1 rounded-full`}
-              ></FaHome>
+              <div className={`${
+                  active === data.id
+                    ? " bg-pink-400 text-white border-[6px] border-white duration-300 w-12 h-12 text-3xl" : "w-10 h-10 text-3xl"
+                } flex justify-center items-center rounded-full p-1`}>
+                {data.icon}
+              </div>
               <p
                 className={`${
-                  active ? "hidden duration-300" : "visible duration-300 mt-1"
+                  active === data.id ? "visible duration-300 mt-1" : "hidden duration-300"
                 } text-center font-bold text-sm text-white`}
               >
-                Home
+                {data.name}
               </p>
             </NavLink>
           </li>)
